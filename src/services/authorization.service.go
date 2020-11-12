8cdeda048fa322135e61126e5e1ded95874656a5
Marc-André Gill <magm102@hotmail.com>
2020-11-11 21:51:11 -0500

    authorization service

--- src/services/authorization.service.go
+++ src/services/authorization.service.go
@@ -1,1 +1,63 @@
 package services
+
+import (
+	"GamesAPI/src/domain"
+	"GamesAPI/src/utils/errorUtils"
+	"context"
+	"net/url"
+	"os"
+)
+
+var (
+	AuthorizationService AuthorizationServiceInterface = NewAuthorizationService(os.Getenv("RBAC_FILEPATH"))
+)
+
+type AuthorizationServiceInterface interface {
+	Authorize(ctx context.Context, url *url.URL, role string, resource string, endpoint string) error
+}
+
+type authorizationService struct {
+	rbac domain.RBAC
+}
+
+//Constructor - must be instantiated with a role-based access YAML file
+func NewAuthorizationService(path string) *authorizationService {
+	ret := &authorizationService{}
+	rbac, err := domain.RbacFromFile(path)
+	if err != nil {
+		panic(err)
+	}
+	ret.rbac = rbac
+	return ret
+}
+
+func (a authorizationService) Authorize(ctx context.Context, url *url.URL, role string, resource string, endpoint string) error {
+	permission, exists := a.rbac[role][resource][endpoint]
+
+	if !exists {
+		return errorUtils.ErrRoleUnknown
+	}
+
+	if !permission.Allow {
+		return errorUtils.ErrForbidden
+	}
+
+	err := permission.Ensure.QueryComplies(ctx, url)
+	if err != nil {
+		return err
+	}
+
+	err = permission.Ensure.QueryComplies(ctx, url)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+
+
+
+
+
